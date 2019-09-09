d4d2accbe633d8d536460e1b90fc76cbf0f835e7
CyprianTinasheAarons <cyprianaarons263@gmail.com>
2019-09-09 16:49:10 +0200

    updated scanner

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,23 @@ if ('serviceWorker' in navigator) {
       })
     })
   }
+
+  function openQRCamera(node) {
+    var reader = new FileReader();
+    reader.onload = function() {
+      node.value = "";
+      qrcode.callback = function(res) {
+        if(res instanceof Error) {
+          alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
+        } else {
+          document.getElementById("qrcode").value = res;
+        }
+      };
+      qrcode.decode(reader.result);
+    };
+    reader.readAsDataURL(node.files[0]);
+  }
+  
+  function showQRIntro() {
+    return confirm("Use your camera to take a picture of a QR code.");
+  }
